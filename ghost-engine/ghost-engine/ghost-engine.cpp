0e5000b73c42e983b9fd998884b7a8b45951365b
Mitch <mitch323@gmail.com>
2016-02-17 13:33:38 -0800

    Implemented rudimentary microcontroller hardware base class: MCHardware

--- ghost-engine/ghost-engine/ghost-engine.cpp
+++ ghost-engine/ghost-engine/ghost-engine.cpp
@@ -15,25 +15,16 @@ class Serial {
 		char *port;
 
     public:
-        //Initialize Serial communication with the given COM port
         Serial(char *portName);
-        //Close the connection
         ~Serial();
-        //Read data in a buffer, if nbChar is greater than the
-        //maximum number of bytes available, it will return only the
-        //bytes available. The function return -1 when nothing could
-        //be read, the number of bytes actually read.
         int ReadData(char *buffer, unsigned int nbChar);
-        //Writes data from a buffer through the Serial connection
-        //return true on success.
         bool WriteData(char *buffer, unsigned int nbChar);
-        //Check if we are actually connected
         bool IsConnected();
 };
 
  Serial::Serial (char *portName) {
 	 //dummy announcements
-	 cout << "Connecting to port USB-" << portName << "\n";
+	 cout << "Connecting to port COM-" << portName << "\n";
 	 port = portName;
 	 connected = true;
 	 cout << "Connection successful.\n";
@@ -55,36 +46,46 @@ class Serial {
 	 return true;
  }
 
-class Radio {
-	bool online, drawer1Open, drawer2Open;
+ class MCHardware {
+  protected:
+	 bool connected;
+  public:
+	bool connect (int);
+	bool isConnected ();
+ };
+
+ bool MCHardware::connect (int port){
+	 //initialize connection at COM-port
+	 return connected;
+ }
+
+ bool MCHardware::isConnected (){
+	return connected;
+}
+
+class Radio: public MCHardware {
+	bool drawers[2];
 	double station;
   public:
 	Radio ();
-	bool isOnline ();
 	bool drawerStatus (int);
 	double tuner ();
 	void openDrawer (int);
 };
 
 Radio::Radio () {
-	online = 1;
-	drawer1Open = drawer2Open = 0;
+	connected = 1;
+	drawers[0] = drawers[1] = false;
+	//These two values are arbitrary
 	station = 96.3;
 	Serial radioSerial("3");
 }
 
-bool Radio:: isOnline (){
-	return online;
-}
-
 bool Radio:: drawerStatus (int drawer){
-	if (drawer == 1){
-		return drawer1Open;
-	}else if (drawer == 2){
-		return drawer2Open;
-	}else {
+	if (drawer > 2){
 		//error
-		return 0;
+	}else {
+		return drawers[drawer+1];
 	}
 }
 
@@ -99,15 +100,10 @@ double Radio:: tuner (){
 }
 
 void Radio:: openDrawer (int drawer){
-	if (drawer == 1){
-		//open drawer 1
+	if (drawer == 1 || drawer == 2){
+		//open drawer
+		drawers[drawer+1] = true;
 		cout << "A drawer on the radio opens, revealing...\n";
-		drawer1Open = 1;
-		return;
-	}else if (drawer == 2){
-		//open drawer 2
-		cout << "Another drawer on the radio opens, revealing...\n";
-		drawer2Open = 1;
 		return;
 	}else {
 		//error
@@ -120,7 +116,7 @@ int _tmain(int argc, _TCHAR* argv[])
 	cout << "Welcome to Seance\n\n";
 
 	Radio radio;
-
+	//Begin radio puzzle
 	cout << "The radio crackles to life and begins playing an odd tune...\n";
 	bool puzzle1Solved = false;
 	double station [3]  = {90.7, 101.7, 99.7};
